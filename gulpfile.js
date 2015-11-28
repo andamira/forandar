30cc61475245503feed392fd1241035696e2f623
joseLuís <joseluis@andamira.net>
2015-11-28 19:24:29 +0100

    rename web folder; update .gitignore; add gulp-gzip

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 var gulp   = require("gulp");
 var dart   = require("gulp-dart");
 var uglify = require("gulp-uglify");
+var gzip   = require("gulp-gzip");
 
 gulp.task("default", function() {
-	return gulp.src('src/*.dart')
+	return gulp.src('web/main.dart')
 	.pipe(dart({
 		"dest": "dist",
-		"minify": "true"
+		"minify": "true",
 	}))
 	.pipe(uglify())
+	.pipe(gzip({ extension: 'gz', gzipOptions: { level: 9 } }))
 	.pipe(gulp.dest('./'))
 });
