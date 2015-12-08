cbb74432be36c6a4262fba493e32fd3835426302
joseLuís <joseluis@andamira.net>
2015-12-08 20:40:29 +0100

    add tests scaffolding; rename main bin file

---
+++ test/bin_test.dart
@@ -0,0 +1,23 @@
+@TestOn("vm")
+
+import "package:test/test.dart";
+
+import "package:forandar/core.dart";
+import "package:forandar/cli.dart";
+
+void main() {
+
+	group("String", () {
+
+		test("String.split() splits the string on the delimiter", () {
+			var string = "foo,bar,baz";
+			expect(string.split(","), equals(["foo", "bar", "baz"]));
+		});
+
+		test("String.trim() removes surrounding whitespace", () {
+			var string = "	foo ";
+			expect(string.trim(), equals("foo"));
+		});
+
+	});
+}
---
+++ test/stack_test.dart
@@ -0,0 +1,22 @@
+import "package:test/test.dart";
+
+import "package:forandar/core.dart";
+
+void main() {
+
+	// TODO
+
+	group("String", () {
+
+		test("String.split() splits the string on the delimiter", () {
+			var string = "foo,bar,baz";
+			expect(string.split(","), equals(["foo", "bar", "baz"]));
+		});
+
+		test("String.trim() removes surrounding whitespace", () {
+			var string = "	foo ";
+			expect(string.trim(), equals("foo"));
+		});
+
+	});
+}
